b40700e0d354226ce5dee6b65bf0420e05fed36a
Lucas Giavaroti <lucas.giavaroti@gmail.com>
2024-02-28 17:30:37 -0300

    Alterções nos temas, tamanhos e layout

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,12 +6,14 @@ let offset = 0;
 
 function convertePokemonToLi(pokemon) {
   return `
-  <li class="pokemon ${pokemon.type}">
-  <span class="number">${pokemon.id}</span>
+  <li class="pokemon ">
+  <span class="number">#0${pokemon.id}</span>
   <span class="name">${pokemon.name}</span>
   <div class="detail">
   <ol class="types">
-  ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join("")}
+  ${pokemon.types
+    .map((type) => `<li class="type ${type}">${type}</li>`)
+    .join("")}
     </ol>
                     <img
                     src=${pokemon.photo}
